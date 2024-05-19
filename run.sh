a5ffb9353913bb721f7f32183e5c4e89eaf2bf3c
youboyo22 <128025927+youboyo22@users.noreply.github.com>
2024-05-19 14:35:36 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
-
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-c04ee113-e8aa-48ad-b046-c70bfc5e7ee6/)
 
 echo "$curl_output" >> Update.tsx
 
